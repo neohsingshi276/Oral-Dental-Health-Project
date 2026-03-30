72b37e00fd8551d960bd9fc92310f463894ea586
Neoh Sing Shi <neohsingshi@gmail.com>
2026-03-30 15:35:30 +0800

    Update email.service.js

--- backend/services/email.service.js
+++ backend/services/email.service.js
@@ -1,7 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp-relay.brevo.com',
+  port: 2525,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
